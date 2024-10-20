8cc5adf05eb521b8e8ad41d67d79569c36e5bd81
Kunal Dabhane <kunaldabhane20012@gmail.com>
2024-10-20 18:47:54 +0530

    -created fast api

--- main.py
+++ main.py
@@ -1,1 +1,11 @@
-print('hello world')
+from fastapi import FastAPI
+
+app = FastAPI()
+
+@app.get("/")
+async def root():
+    return {"message": "Hello, world!"}
+
+if __name__ == "__main__":
+    import uvicorn
+    uvicorn.run("main:app", host="0.0.0.0", port=8000)
