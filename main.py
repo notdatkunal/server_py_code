d24524c646b04e762a9938b6f6a2a25bf0f4721e
Kunal Dabhane <kunaldabhane20012@gmail.com>
2024-10-20 20:30:07 +0530

    ui components updated

--- main.py
+++ main.py
@@ -1,34 +1,38 @@
 from fasthtml.common import *
 
-app,rt = fast_app()
+app, rt = fast_app()
+
 
 @dataclass
 class Link:
-    title:str
-    link:str
+    title: str
+    link: str
 
-my_list :list[Link]= [
-    Link(title='instagram',link='https://www.instagram.com/notdatkunal')
-    ,Link(title='X',link='https://www.X.com/notdatkunal')
-    ,Link(title='github',link='https://www.github.com/notdatkunal')
-    ,Link(title='youtube',link='https://www.youtube.com/notdatkunal')
+
+my_list: list[Link] = [
+    Link(title='instagram', link='https://www.instagram.com/notdatkunal')
+    , Link(title='X', link='https://www.X.com/notdatkunal')
+    , Link(title='github', link='https://www.github.com/notdatkunal')
+    , Link(title='youtube', link='https://www.youtube.com/notdatkunal')
 
 ]
 
+
 @rt('/links')
 def get():
-    head = Ul(
+    head = Ol(
         *[
-            Li(A(item.title.capitalize(),href=item.link,target_id='_blank')) for item in my_list
+            Li(A(item.title.capitalize(), href=item.link, hx_target='_blank')) for item in my_list
         ]
 
     )
 
-    return Div(head,Titled(P(f'this is links page')),A('go back',href='/'))
+    return Div(head, Titled(P(f'this is links page')), A('go back', href='/'))
 
 
 @rt('/')
 def get():
-    return Div(P(f'Welcome to my application'),A('go to links page',href='/links'), hx_get="/change")
+    return Div(P(f'Welcome to my application'), A('go to links page', href='/links'), hx_get="/change")
+
 
 serve(port=8000)
