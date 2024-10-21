b11ab3736b35f22d50600bd4b707d106ed01231b
Kunal Dabhane <kunaldabhane20012@gmail.com>
2024-10-21 07:45:36 +0530

    - updated shell script

--- start.sh
+++ start.sh
@@ -6,6 +6,6 @@ virtualenv_name="venv"
 source "$virtualenv_name/bin/activate"
 # Specify the path to your Python file
 python_file="main.py"  # Replace with the actual file name
-
+pip install -r requirements.txt
 # Run the Python file
 $python_interpreter "$python_file"
